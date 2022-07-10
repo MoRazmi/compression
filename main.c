57ce1dbad63f19294b5d16ce7f3ccc1f3f477adb
U-LAPTOP-1BRIDO5P\monar <mohammadsalehrazmi@gmail.com>
2022-07-10 12:06:47 -0400

    Add interview data_ptr and data_size in main.c
    * Add and print data from interview:

--- main.c
+++ main.c
@@ -5,11 +5,21 @@
  *      Author: Mo Razmi
  */
 #include <stdio.h>
+#include <stddef.h>
 #include "compress_run_length.h"
 
 int main()
 {
+	uint8_t data_ptr[] = { 0x03, 0x74, 0x04, 0x04, 0x04, 0x35, 0x35, 0x64,
+	                0x64, 0x64, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
+	                0x56, 0x45, 0x56, 0x56, 0x56, 0x09, 0x09, 0x09 };
 
+	size_t data_size = 24;
+
+	for (int i = 0; i < data_size; i++)
+	{
+		printf("index %d is 0x%02X \n", i, data_ptr[i]);
+	}
 	printf("Let's start the interview\n");
 	return 0;
 }
